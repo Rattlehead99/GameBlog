fb1c90e08a5514316b828e058846660fd0063188
Rattlehead99 <ikovachev99@gmail.com>
2022-03-21 16:52:34 +0200

    Add project files.

---
+++ GameBlog/Data/GameBlogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameBlog.Data
+{
+    public class GameBlogDbContext : IdentityDbContext
+    {
+        public GameBlogDbContext(DbContextOptions<GameBlogDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
