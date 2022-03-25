3dc327c75c5a89e89ad77de3eee03c4f71f10f85
Rattlehead99 <ikovachev99@gmail.com>
2022-03-25 22:30:08 +0200

    Created the Initial Profile page TODO: Proper profile page Postponed the comment functionality

---
+++ GameBlog/Models/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GameBlog.Data.Models;
+
+namespace GameBlog.Models
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+
+        public int Reputation { get; set; }
+
+        public string UserName { get; set; }
+
+        public  string Email { get; set; }
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+        public ICollection<Rating> Ratings { get; set; } = new List<Rating>();
+
+        public ICollection<Article> Articles { get; set; } = new List<Article>();
+    }
+}
